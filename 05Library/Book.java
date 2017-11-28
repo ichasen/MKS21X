2d59b06a8a151fbebd37e2db3dc73e7b9e681b5b
ichasen <ichasen@stuy.edu>
2017-11-28 15:23:05 -0500

    Added Book.java full class

---
+++ 05Library/Book.java
@@ -0,0 +1,34 @@
+public class Book{
+    private String author;
+    private String title;
+    private int ISBN;
+    public null Book (){}
+    public Book (String a, String t, int n){
+	author = a;
+	title = t;
+	ISBN = n;
+    }
+    public int getISBN(){
+	return ISBN;
+    }
+    public String getAuthor(){
+	return author;
+    }
+    public String getTitle(){
+	return title;
+    }
+    public void setTitle (String newTitle){
+	title = newTitle;
+    }
+    public void setAuthor(String newAuthor){
+	author = newAuthor;
+    }
+    public void setISBN(int num){
+	ISBN = num;
+    }
+    public String toString (){
+	return title + ", " + author + ", " + ISBN;
+    }
+}
+    
+    
