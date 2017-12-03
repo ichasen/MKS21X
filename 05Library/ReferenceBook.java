78c1a5df9a1085b761477d8d62a4731df09d769c
ichasen <33767448+ichasen@users.noreply.github.com>
2017-12-03 13:32:28 -0500

    Add files via upload

---
+++ 05Library/ReferenceBook.java
@@ -0,0 +1,30 @@
+public class ReferenceBook extends LibraryBook{
+    private String collection;
+    public ReferenceBook(String a, String t, String cn, int n, String collection){
+	super(a,t,n,cn);
+	this.collection = collection;
+    }
+    public String getCollection(){
+	return collection;
+    }
+    public String setCollection(String newCollection){
+	collection = newCollection;
+	return newCollection;
+    }
+    public void checkout(String patron, String due){
+	System.out.println ("cannot check out a reference book");
+    }
+    public void returned(){
+	System.out.println ("reference book could not have been checked out - return impossible");
+    }
+    public String circulationStatus(){
+	String ans = "non-circulating reference book";
+	return ans;
+    }
+    public String toString(){
+	return getTitle() + ", " + getAuthor() + ", " + getISBN() + ", " + getCallNumber() + ", " + circulationStatus() + ", " + checkout() + ", " + returned();
+    }
+}
+    
+	
+	
