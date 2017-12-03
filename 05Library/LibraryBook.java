ad4f9c91722748c7c769b3ea68e4e5c379701c00
ichasen <33767448+ichasen@users.noreply.github.com>
2017-12-03 10:34:18 -0500

    Add files via upload

---
+++ 05Library/LibraryBook.java
@@ -0,0 +1,23 @@
+public abstract class LibraryBook extends Book implements Comparable<LibraryBook>{
+    private String callNumber;
+    public LibraryBook(String a, String t, int n, String cn){
+	super(a,t,n);
+	callNumber = cn;
+    }
+    public String getCallNumber (){
+	return callNumber;
+    }
+    public String setCallNumber (String newNumber){
+	callNumber = newNumber;
+	return newNumber;
+    }
+    public abstract void checkout(String patron, String due);
+    public abstract void returned();
+    public abstract String circulationStatus();
+    public int compareTo (String other){
+	return callNumber.compareTo(other);
+    }
+    public String toString(){
+	return getTitle() + ", " + getAuthor() + ", " + getISBN() + ", " + getCallNumber() + ", " + circulationStatus();
+    }
+}
