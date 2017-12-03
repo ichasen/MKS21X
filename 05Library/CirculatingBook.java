eba3e86413b16afc3afced9aca9b4982ea747558
ichasen <33767448+ichasen@users.noreply.github.com>
2017-12-03 14:04:40 -0500

    Add files via upload

---
+++ 05Library/CirculatingBook.java
@@ -0,0 +1,48 @@
+public class CirculatingBook extends LibraryBook{
+    private String currentHolder;
+    private String dueDate;
+    public CirculatingBook(String a, String t, int n, String cn){
+	super (a,t,n,cn);
+	currentHolder = null;
+	dueDate = null;
+    }
+    public String getCurrentHolder(){
+	return currentHolder;
+    }
+    public String getDueDate(){
+	return dueDate;
+    }
+    public String setCurrentHolder(String newHolder){
+        currentHolder = newHolder;
+	return newHolder;
+    }
+    public String setDueDate(String newDueDate){
+	dueDate = newDueDate;
+	return newDueDate;
+    }
+    public void checkout(String patron, String due){
+	currentHolder = patron;
+	dueDate = due;
+    }
+    public void returned(){
+	currentHolder = null;
+	dueDate = null;
+    }
+    public String circulationStatus(){
+	ans = "";
+	if (currentHolder.equals(null)){
+	    ans = "book available on the shelves";
+	}
+	else{
+	    ans = currentHolder + ", due on" + dueDate;
+	}
+	return ans;
+    }
+    public String toString(){
+	return getTitle() + ", " + getAuthor() + ", " + getISBN() + ", " + getCallNumber() + ", " + circulationStatus() + ", " + getCurrentHolder() + ", " + getDueDate();
+    }
+}
+	    
+	
+	
+	
